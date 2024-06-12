1ec67d4ebf89153fc61f8fb4905ad3d5ac29e366
Blugamma <blugammadesign@gmail.com>
2024-06-12 17:11:51 +0100

    initial commit

---
+++ wwwroot/js/Shopify.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    const client = window.ShopifyBuy.buildClient({
+        domain: 'www.gingerfox.co.uk',
+        storefrontAccessToken: '5f230d2d8cbccf9bbe5d60551645777f'
+    });
+
+
+    $("#BuyNowBtnBanner").click(function () {
+        //const input = {
+        //    lineItems: [{
+        //        variantId: 'gid://shopify/ProductVariant/47080005861662',
+        //        quantity: 1,
+        //        customAttributes: [{ key: "Microsite", value: "Catchphrase" }]
+        //    }],
+        //}
+        //client.checkout.create(input).then((checkout) => {
+        //    location.href = checkout.webUrl;
+        //});
+
+        location.href = "https://www.gingerfox.co.uk/products/official-catchphrase-say-what-you-see-special-edition-card-game";
+    });
+
+    $("#BuyNowBtn").click(function () {
+        //const input = {
+        //    lineItems: [{
+        //        variantId: 'gid://shopify/ProductVariant/47080005861662',
+        //        quantity: 1,
+        //        customAttributes: [{ key: "Microsite", value: "Catchphrase" }]
+        //    }]
+        //}
+        //client.checkout.create(input).then((checkout) => {
+        //    location.href = checkout.webUrl;
+        //});
+
+        location.href = "https://www.gingerfox.co.uk/products/official-catchphrase-say-what-you-see-special-edition-card-game";
+    });
+})
